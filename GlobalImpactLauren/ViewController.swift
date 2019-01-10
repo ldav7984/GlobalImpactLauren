13fd92678e06fdfaaeb0e401fc5d491de8538803
Davis, Lauren <ldav7984@740s-pl-109.local>
2019-01-10 12:14:50 -0700

    Initial Commit

---
+++ GlobalImpactLauren/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GlobalImpactLauren
+//
+//  Created by Davis, Lauren on 1/10/19.
+//  Copyright © 2019 Davis, Lauren. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
