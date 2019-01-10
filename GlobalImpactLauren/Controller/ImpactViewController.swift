31232636b2cdae86789299f20564b30c24defc64
Davis <ldav7984@740s-pl-109.local>
2019-01-10 12:43:50 -0700

    Programmed the ImpactViewController.

--- GlobalImpactLauren/Controller/ImpactViewController.swift
+++ GlobalImpactLauren/Controller/ImpactViewController.swift
@@ -7,14 +7,37 @@
 //
 
 import UIKit
+import MapKit
 
-class ImpactViewController: UIViewController {
-
-    override func viewDidLoad() {
+public class ImpactViewController: UIViewController
+{
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var AnimateButton: UIButton!
+    @IBOutlet weak var Image: UIImageView!
+    
+    public override func viewDidLoad() -> Void
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func AnimateButton(_ sender: UIButton)
+    {
+        let impactLocation = CLLocationCoordinate2D(latitude: 5, longitude: 95)
+        let mapSpan = MKCoordinateSpan(latitudeDelta: 10.05, longitudeDelta: 10.05)
+        let impactRegion = MKCoordinateRegion(center: impactLocation, span: mapSpan)
+        mapView.animatedZoom(zoomRegion: impactRegion, duration: 2.5)
+    }
+    
+    
+}
 
+
+extension MKMapView
+{
+    public func animatedZoom(zoomRegion:MKCoordinateRegion, duration:TimeInterval)
+    {
+        MKMapView.animate(withDuration: duration, delay: 0, usingSpringWithDamping: 0.6, initialSpringVelocity: 10, options: UIView.AnimationOptions.curveEaseIn, animations: { self.setRegion(zoomRegion, animated: true)}, completion: nil)
+    }
 }
 
